fc67d9a999144c575c3422182076784a37232c56
Gean <a2019114321@estudiantes.upsa.edu.bo>
2025-07-20 23:42:00 -0400

    Corregir Mejores Vendedores

--- crmcliente/pages/mejoresvendedores.js
+++ crmcliente/pages/mejoresvendedores.js
@@ -1,8 +1,8 @@
-import React, {useEffect} from 'react';
-import Layout from '../components/Layout'
+import React, { useEffect } from 'react';
+import Layout from '../components/Layout';
 import {
-    BarChart, Bar,  XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
-  } from 'recharts';
+    BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
+} from 'recharts';
 import { gql, useQuery } from '@apollo/client';
 
 const MEJORES_VENDEDORES = gql`
@@ -18,53 +18,32 @@ const MEJORES_VENDEDORES = gql`
 `;
 
 const MejoresVendedores = () => {
-
-    const {data, loading, error, startPolling, stopPolling} = useQuery(MEJORES_VENDEDORES);
-
-  
+    const { data, loading, error, startPolling, stopPolling } = useQuery(MEJORES_VENDEDORES);
 
     useEffect(() => {
         startPolling(1000);
-        return () => {
-            stopPolling();
-        }
-    }, [startPolling, stopPolling])
+        return () => stopPolling();
+    }, [startPolling, stopPolling]);
 
-    if(loading) return 'cargando...';
+    if (loading) return 'Cargando...';
+    if (error) return <p className="text-red-600">Error al cargar los datos</p>;
 
-    console.log(data);
+    const { mejoresVendedores } = data;
 
-
-    const { mejoresVendedores} = data;
-    
-    const vendedorGrafica = [];
-
-     mejoresVendedores.map((vendedor, index) => {
-        vendedorGrafica[index] = {
-            ...vendedor.vendedor[0],
-            total: vendedor.total
-        }
-    })
-
-
-    console.log(vendedorGrafica);
+    const vendedorGrafica = mejoresVendedores.map(({ vendedor, total }) => ({
+        ...vendedor,
+        total
+    }));
 
     return (
         <Layout>
             <h1 className="text-2xl text-gray-800 font-light">Mejores Vendedores</h1>
 
-            <ResponsiveContainer
-                width={'99%'}
-                height={550}
-            >
+            <ResponsiveContainer width={'99%'} height={550}>
                 <BarChart
                     className="mt-10"
-                    width={600}
-                    height={500}
                     data={vendedorGrafica}
-                    margin={{
-                        top: 5, right: 30, left: 20, bottom: 5,
-                    }}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
                 >
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="nombre" />
@@ -75,7 +54,7 @@ const MejoresVendedores = () => {
                 </BarChart>
             </ResponsiveContainer>
         </Layout>
-     );
-}
- 
+    );
+};
+
 export default MejoresVendedores;
